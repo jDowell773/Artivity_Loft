674cfba4a5beccb2438f1d7be22cdd535156ef76
JonathanDowell <mr.jonathandowell@gmail.com>
2020-05-10 11:44:04 -0500

    add backend authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,14 +1,18 @@
 class Api::UsersController < ApplicationController
 
   def create
-    @user = User.new(
+    user = User.new(
       user_name: params[:user_name],
       profile: params[:profile],
       email: params[:email],
       password_digest: params[:password_digest]
     )
-    @user.save
-    render "show.json.jb"
+    
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     
     post "/users" => "users#create"
 
+    post "/sessions" => "sessions#create"
+
    end
 end
 
